3ef3e56af65d8e666af21b08f3d77925dab0092e
João Victor Falcão Ribeiro <106679750+FalcaoBranco@users.noreply.github.com>
2023-07-22 11:03:00 -0300

    Delete lista10,02.py

--- lista10,02.py
+++
@@ -1,9 +0,0 @@
-turno = input('Em qual turno voce trabalha digite M para matutino V para vespertino e N para noturno')
-if turno == 'm' or turno == 'M':
-    print('Bom dia')
-elif turno == 'v' or turno == 'V':
-    print('Boa tarde')
-elif turno == 'n' or turno == 'N':
-    print('Boa noite')
-else:
-    print('Voce digitou algo que não estava na lista erro')
