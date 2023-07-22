73fe575a90c2341fa7c344bd35dcd0ba7874595f
João Victor Falcão Ribeiro <106679750+FalcaoBranco@users.noreply.github.com>
2023-07-22 11:02:48 -0300

    Delete lista09,02.py

--- lista09,02.py
+++
@@ -1,9 +0,0 @@
-numero01 = float(input('Digite um numero: '))
-numero02 = float(input('Digite outro numero: '))
-numero03 = float(input('Digite um ultimo numero: '))
-numeros = [numero01,numero02,numero03]
-numeros.sort(reverse=True)
-print('Os numeros em hordem decresente são:')
-for numero in numeros:
-    print(numero)
-    
