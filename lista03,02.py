0320a4124d912c803e6d66fd250f7174c78b80c1
João Victor Falcão Ribeiro <106679750+FalcaoBranco@users.noreply.github.com>
2023-07-22 11:01:29 -0300

    Delete lista03,02.py

--- lista03,02.py
+++
@@ -1,7 +0,0 @@
-letra = input('Digite uma letra: ')
-if letra == 'f' or letra == 'F':
-    print(f'A letra digitada é {letra}')
-elif letra == 'm' or  letra == 'M':
-    print(f'A letra digitada é {letra}')
-else:
-    print('A letra digitada não é F nem M  erro')
