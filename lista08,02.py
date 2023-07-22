937c673a1cbeef3d3e9bde3465c40c0be418e185
João Victor Falcão Ribeiro <106679750+FalcaoBranco@users.noreply.github.com>
2023-07-22 11:02:40 -0300

    Delete lista08,02.py

--- lista08,02.py
+++
@@ -1,5 +0,0 @@
-produto01 = float(input('Qual o preço do primeiro produto: '))
-produto02 = float(input('Qual o preço do segundo produto: '))
-produto03 = float(input('Qual o preço do ultimo produto: '))
-mais_barato = min(produto01,produto02,produto03)
-print(F'O produto que voce deve comprar é pois ele é o mais barato custando somente R$:{mais_barato}')
