eaccd12ca14a171030cba5c54665cd5ed7f52899
João Victor Falcão Ribeiro <106679750+FalcaoBranco@users.noreply.github.com>
2023-07-22 11:03:31 -0300

    Delete lista18.py

--- lista18.py
+++
@@ -1,8 +0,0 @@
-Tamanho_arquivo = float(input('Qual é o tamanho do arquivo em MB? : '))
-Velocidade_internet = float(input('Qual é a velocidade da internet? : '))
-
-tamanho_bits = Tamanho_arquivo * 8 * 1024 * 1024
-tempo_segundos = tamanho_bits / (Velocidade_internet * 1000000)
-tempo_minutos = tempo_segundos / 60
-
-print(f'O tempo aprocimado do dawnload é de {tempo_minutos} Minutos')
